4f90398a811150d5ffc0562d373add4436501e18
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-01 01:44:21 -0700

    started writing service worker

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
+    "/db.js"
+]
