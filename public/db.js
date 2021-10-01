fe438ae163696b75fab7cae6dac5a300d072c643
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-09-30 19:57:16 -0700

    added needed functions and pseudocode

--- public/db.js
+++ public/db.js
@@ -3,3 +3,58 @@ let budgetVersion;
 
 // Opening new database request
 const request = indexedDB.open('BudgetDB', budgetVersion || 1);
+
+
+request.onupgradeneeded = function (e) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+};
+
+request.onsuccess = function (e) {
+    db = e.target.result;
+
+    // Check if online before reading from db
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+}
+
+request.onerror = function (e) {
+    console.log(`BORK. ${e.target.errorCode}`)
+}
+
+function saveRecord(record) {
+    // Create txn on BudgetStore db with readwrite access
+    // Access BudgetStore object store
+    // Add record to store with add method
+}
+
+function checkDatabase() {
+    // Open txn on BedgetStore db
+    // Access BudgetStore object
+    // Get all records from store and set to variable
+
+    // If successful:
+    getAll.onsuccess = function () {
+        // Check for items and bulk add when coming back online
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    // If success, open txn on BudgetStore db with readwrite access
+                    // Access pending object store
+                    // Clear store
+                });
+        }
+    };
+}
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
+
